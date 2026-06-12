c9b5bb95cfa4a402be41c786be39301b13eef196
Sandeep Pandit <sandippandit896@gmail.com>
2026-06-12 14:26:11 +0530

    a

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".home", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      navigation:{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
+  });
+});
